0bb4b8ae8af0349831f8f808d16e3d55378cd9d3
Lococo <anthony.lococo@jedaiidev.com>
2024-05-07 17:38:44 +0200

    testconnection

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 router.get('/movies', (req, res) => {
-	fetch(`https://api.themoviedb.org/3/discover/movie?api_key=850ceff6ade337e674661e8363c155a3`)
+	fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
 		.then(response => response.json())
 		.then(data => {
 			res.json({ movies: data.results });
